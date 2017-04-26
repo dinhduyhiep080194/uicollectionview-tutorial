9f2d6612f525cc0f04f1f588a70b99ebf0be776d
dinhduyhiep080194 <dinhduyhiep1@gmail.com>
2017-04-26 16:23:33 +0700

    Add files via upload

---
+++ FlickrSearch/FlickrPhotoCellCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  FlickrPhotoCellCollectionViewCell.swift
+//  FlickrSearch
+//
+//  Created by cntt17 on 4/26/17.
+//  Copyright © 2017 cntt17. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotoCellCollectionViewCell: UICollectionViewCell {
+    
+}
